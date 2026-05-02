88e91c99067789599de3b3b0a561b26c7b025cd4
FFBC Admin <admin@firstfriendship.com>
2026-05-02 00:47:27 -0700

    Remove duplicate interface definition in ObjC plugin macro for Build #73

--- local-plugins/storekit-paywall/ios/PluginObjc/StoreKitPaywallPlugin.m
+++ local-plugins/storekit-paywall/ios/PluginObjc/StoreKitPaywallPlugin.m
@@ -1,9 +1,6 @@
 #import <Foundation/Foundation.h>
 #import <Capacitor/Capacitor.h>
 
-@interface StoreKitPaywallPlugin : CAPPlugin
-@end
-
 CAP_PLUGIN(StoreKitPaywallPlugin, "StoreKitPaywall",
     CAP_PLUGIN_METHOD(presentPaywall, CAPPluginReturnPromise);
 )
